df3ad9734e32962c5fe6aac6cd7a9acae541654e
SHRI KANT <127958886+shrIKant-07@users.noreply.github.com>
2023-03-15 22:38:53 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,9 @@
+public class table {
+    public static void main(String[] args) {
+       int a=2;
+	   int b=3;
+        for (int i = 1, j = 10; i <= 10 && j > 0; i++, j--) {
+            System.out.println(a + "*" + i + "= " + a*i + " :: " + b + "*" + j + "= " + + b*j);
+        }
+    }
+}
